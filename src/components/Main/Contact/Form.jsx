203c783b247d550ea48278a4eecb08ab5e2be930
LeandroMassola <leandromassola.lm@gmail.com>
2025-10-03 18:23:24 +0200

    terminado, para hacer pull req a main

--- src/components/Main/Contact/Form.jsx
+++ src/components/Main/Contact/Form.jsx
@@ -1,17 +1,30 @@
 import { useState } from "react"
 import {motion} from "framer-motion"
+import validations from "./validationForm"
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, Controller } from "react-hook-form";
 
 export default function Form() {
 
     const [statusSend, setStatusSend] = useState(null)
 
-    const [formData, setFormData] = useState({
+    /* const [formData, setFormData] = useState({
         name:"",
         surname:"",
         mail:"",
         subject:"",
         message:""
-    })
+    }) */
+
+        const { handleSubmit, reset, control, setError, formState:{errors, isSubmitting},} = useForm({
+            resolver:yupResolver(validations), defaultValues: {
+                name: "",
+                surname: "",
+                mail: "",
+                subject:"",
+                message: "",
+            }
+        })
 
     function handleChange(e) {
         const {name,value} = e.target;
@@ -21,35 +34,38 @@ export default function Form() {
         })
     }
 
-    function handleSubmit(e) {
+    async function submitForm(data) {
 
         e.preventDefault()
         setStatusSend("Enviando ...")
 
-        fetch("/api/contactApi", {
-            method:"POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(formData)
-        })
-        .then((res)=>res.json())
-        .then((data)=>{
-            if (data.success) {
-                setStatusSend("Mensaje enviado con éxito")
-                setFormData({
-                    name:"",
-                    surname:"",
-                    mail:"",
-                    subject:"",
-                    message:"",
-                })
+
+        try {
+            const response = await fetch("/api/contactApi", {
+                method:"POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(data)
+            }) 
+
+            const json = await response.json()
+
+            if(response.ok && json.success) {
+                setStatusSend("¡Mensaje enviado con éxito!");
+                reset();
             } else {
-                setStatusSend("Eror al enviar el mensaje")
+                if (json?.error?.field) {
+                    setError(json.error.field, { type: "server", message: json.error.message });
+                    setStatusSend(" Error al enviar");
+                } else {
+                        setStatusSend("Error al enviar el mensaje");
+                }
             }
+        } catch (error) {
+            setStatusSend(` Error de conexión: ${error.message}`);
+        }
+        
             
-        })
-        .catch((err)=> {
-            setStatusSend(`Error al enviar: ${err}`)
-        })
+        
     }
 
     return(
@@ -60,27 +76,72 @@ export default function Form() {
             <h1 className="text-center text-5xl z-50 relative mt-20 text-[var(--afterglow)] sm:text-6xl md:text-7xl xl:self-center xl:pb-10">Escribínos!</h1>
 
             {statusSend != null && <h3>{statusSend}</h3>}
-            <form className="relative mt-20 flex flex-col justify-center items-center" method="POST" action="#" onSubmit={handleSubmit}>
+            <form className="relative mt-20 flex flex-col justify-center items-center" method="POST" action="#" onSubmit={handleSubmit(submitForm)}>
                 <div  className="flex flex-col justify-center items-center gap-3 w-full h-full">
                     <label className="flex text-[var(--afterglow)] tracking-widest font-extralight text-lg flex-col text-center sm:text-xl" htmlFor="name">Nombre
-                        <motion.input whileFocus={{backgroundColor:"var(--afterglow)", boxShadow:"0 0 0 5px var(--afterglow)"}} whileHover={{outline:"2px solid var(--afterglow)", boxShadow:"0 0 0 3px var(--afterglow)", backgroundColor:"var(--afterglow)"}} className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" onChange={handleChange} type="text" name="name" placeholder="Nombre" />
+                        <Controller 
+                        name={"name"} 
+                        control={control} 
+                        render={({ field }) => (
+                            <motion.input {...field} whileFocus={{backgroundColor: "var(--afterglow)", boxShadow: "0 0 0 5px var(--afterglow)",}}whileHover={{outline: "2px solid var(--afterglow)", boxShadow: "0 0 0 3px var(--afterglow)", backgroundColor: "var(--afterglow)",}}className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" type="text" placeholder="Nombre"/>)}
+                        />
+                        
+
                     </label>
+                    {errors.name && <p className="text-xl mt-5 text-[var(--spectraYellow)] motion-safe:animate-pulse">{errors.name.message}</p>}
 
                     <label className="flex text-[var(--afterglow)] tracking-widest font-extralight text-lg flex-col text-center sm:text-xl" htmlFor="surname">Apellido
-                        <motion.input whileFocus={{backgroundColor:"var(--afterglow)", boxShadow:"0 0 0 5px var(--afterglow)"}} whileHover={{outline:"2px solid var(--afterglow)", boxShadow:"0 0 0 3px var(--afterglow)", backgroundColor:"var(--afterglow)"}} className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" onChange={handleChange} type="text" name="surname" placeholder="Apellido" />
+                        <Controller
+                        name={"surname"} 
+                        control={control} 
+                        render={({ field }) => (
+                            <motion.input {...field} whileFocus={{backgroundColor: "var(--afterglow)", boxShadow: "0 0 0 5px var(--afterglow)",}}whileHover={{outline: "2px solid var(--afterglow)", boxShadow: "0 0 0 3px var(--afterglow)", backgroundColor: "var(--afterglow)",}}className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" type="text" placeholder="Apellido"/>)}
+                        />
+                            
                     </label>
+                    {errors.surname && <p className="text-xl mt-5 text-[var(--spectraYellow)] motion-safe:animate-pulse">{errors.surname.message}</p>}
+
+                    
 
                     <label className="flex text-[var(--afterglow)] tracking-widest font-extralight text-lg flex-col text-center sm:text-xl" htmlFor="mail">Mail
-                        <motion.input whileFocus={{backgroundColor:"var(--afterglow)", boxShadow:"0 0 0 5px var(--afterglow)"}} whileHover={{outline:"2px solid var(--afterglow)", boxShadow:"0 0 0 3px var(--afterglow)", backgroundColor:"var(--afterglow)"}} className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" onChange={handleChange} type="email" name="mail" placeholder="Mail" />
+                        <Controller 
+                        name={"mail"} 
+                        control={control} 
+                        render={({ field }) => (
+                            <motion.input {...field} whileFocus={{backgroundColor: "var(--afterglow)", boxShadow: "0 0 0 5px var(--afterglow)",}}whileHover={{outline: "2px solid var(--afterglow)", boxShadow: "0 0 0 3px var(--afterglow)", backgroundColor: "var(--afterglow)",}}className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" type="text" placeholder="Mail"/>)}
+                        />
+                        
                     </label>
+                    {errors.mail && <p className="text-xl mt-5 text-[var(--spectraYellow)] motion-safe:animate-pulse">{errors.mail.message}</p>}
+
+                    
 
                     <label className="flex text-[var(--afterglow)] tracking-widest font-extralight text-lg flex-col text-center sm:text-xl" htmlFor="subject">Asunto
-                        <motion.input whileFocus={{backgroundColor:"var(--afterglow)", boxShadow:"0 0 0 5px var(--afterglow)"}} whileHover={{outline:"2px solid var(--afterglow)", boxShadow:"0 0 0 3px var(--afterglow)", backgroundColor:"var(--afterglow)"}} className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" onChange={handleChange} type="text" name="subject" placeholder="Asunto" />
+                        <Controller 
+                        name={"subject"} 
+                        control={control} 
+                        render={({ field }) => (
+                            <motion.input {...field} whileFocus={{backgroundColor: "var(--afterglow)", boxShadow: "0 0 0 5px var(--afterglow)",}}whileHover={{outline: "2px solid var(--afterglow)", boxShadow: "0 0 0 3px var(--afterglow)", backgroundColor: "var(--afterglow)",}}className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" type="text" placeholder="Asunto"/>)}
+                            />      
+
                     </label>
+                    {errors.subject && <p className="text-xl mt-5 text-[var(--spectraYellow)] motion-safe:animate-pulse">{errors.subject.message}</p>} 
+
+                    
 
                     <label className="flex text-[var(--afterglow)] tracking-widest font-extralight text-lg flex-col text-center sm:text-xl" htmlFor="message">Mensaje
-                        <motion.textarea whileFocus={{backgroundColor:"var(--afterglow)", boxShadow:"0 0 0 5px var(--afterglow)"}} whileHover={{outline:"2px solid var(--afterglow)", boxShadow:"0 030 5px var(--afterglow)", backgroundColor:"var(--afterglow)"}} className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" onChange={handleChange} name="message" placeholder="Mensaje" />
+                        <Controller 
+                        name={"message"} 
+                        control={control} 
+                        render={({ field }) => (
+                            <motion.input {...field} whileFocus={{backgroundColor: "var(--afterglow)", boxShadow: "0 0 0 5px var(--afterglow)",}}whileHover={{outline: "2px solid var(--afterglow)", boxShadow: "0 0 0 3px var(--afterglow)", backgroundColor: "var(--afterglow)",}}className="w-[80%] ml-5 py-3 px-5 outline-none text-[var(--andorra)] border-none  bg-gray-300 rounded-md" type="text" placeholder="Mensaje"/>)}
+                        />        
+                        
                     </label>
+                    {errors.message && <p className="text-xl mt-5 text-[var(--spectraYellow)] motion-safe:animate-pulse">{errors.message.message}</p>}
+
+                    
+
                 </div>
 
                 <div className="flex px-15 mt-20 justify-center gap-20 items-center">
